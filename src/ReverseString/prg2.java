960b98070127cae8184b3801b7dc96d6f9d4da55
bhoom <bhoom@Bhoomika>
2024-08-15 14:24:27 +0530

    commit prg2 push

---
+++ src/ReverseString/prg2.java
@@ -0,0 +1,17 @@
+package ReverseString;
+
+import org.testng.annotations.Test;
+
+public class prg2 {
+	@Test
+	public void num()
+	{
+	int a=10;
+	int b=20;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println(a);
+	System.out.println(b);
+	}
+}
